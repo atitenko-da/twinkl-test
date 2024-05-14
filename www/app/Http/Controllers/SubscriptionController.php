c8dc1df42bf6c697f230fd65a25a9bd9e69badfb
atitenko-da <atitenko@dataart.com>
2024-05-14 12:19:40 +0300

    refactoring, validation in FormRequest

--- www/app/Http/Controllers/SubscriptionController.php
+++ www/app/Http/Controllers/SubscriptionController.php
@@ -2,14 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
-use App\Enum\UserTypeEnum;
 use App\Models\Subscription;
-use Illuminate\Validation\Rule;
-use Illuminate\Support\Facades\Validator;
 use App\Services\MailService\MailService;
+use App\Http\Requests\SubscriptionRequest;
 
 class SubscriptionController extends Controller
 {
@@ -17,20 +14,9 @@ class SubscriptionController extends Controller
         protected MailService $mailService,
     ) {}
 
-    public function store(Request $request): JsonResponse|RedirectResponse
+    public function store(SubscriptionRequest $request): JsonResponse|RedirectResponse
     {
-        $validator = Validator::make($request->all(), [
-            'first_name' => ['required', 'alpha:ascii', 'max:255'],
-            'last_name' => ['required', 'alpha:ascii', 'max:255'],
-            'email' => ['required', 'email:rfc,dns', 'unique:subscriptions', 'max:100'],
-            'type' => ['required', Rule::enum(UserTypeEnum::class)],
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
-        }
-
-        $validated = $validator->validated();
+        $validated = $request->validated();
         $subscription = Subscription::create($validated);
         $message = $this->mailService->createMessage($subscription);
         $this->mailService->sendMessage($message);
